bd2795f32a5c7d0b79fc6c37b77eb8bd20dc0ebc
Xueming Yin <yxm1277@gmail.com>
2019-09-29 11:08:40 -0400

    update profile functionality

--- teachers-website/src/app/components/profile/profile.component.ts
+++ teachers-website/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user-service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  
+  public username;
+  constructor(private userService: UserService ) { }
 
   ngOnInit() {
+    this.username = this.userService.getStoredLoggedInUsername();
   }
+  
 
 }
