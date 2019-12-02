9cb3d7a62f0ffaeefe121022de2d5b998ca033ec
nestornorton <nestormoreno44@gmail.com>
2019-12-01 22:40:54 -0500

    CSS changes and viewing component not in navbar, only instantiated by clicking on post or search result
    Signed-off-by: nestornorton <nestormoreno44@gmail.com>

--- teachers-website/src/app/components/viewing/viewing.component.ts
+++ teachers-website/src/app/components/viewing/viewing.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {PostsContentService} from '../../services/posts-content.service';
 import {UserService} from '../../services/user-service';
 import {DomSanitizer} from '@angular/platform-browser';
@@ -20,24 +20,18 @@ export class ViewingComponent implements OnInit {
 
   contentArray: any[] = [];
 
-  imageArray: any[] = [];
-  imageURL: any;
-
-  pdfArray: any[] = [];
-
-  videoArray: any[] = [];
-
-  // @ViewChild('preview') preview: ElementRef;
-
   constructor(
     private activatedRoute: ActivatedRoute, private userService: UserService,
-    private postContentService: PostsContentService, private sanitizer: DomSanitizer
+    private postContentService: PostsContentService, private router: Router
   ) {
     this.getRouteParams();
   }
 
 
   ngOnInit() {
+    if (!this.userService.isLoggedIn) {
+      this.router.navigate(['/Login']);
+    }
     // Get data from MongoDB then put in contentArray
     this.postContentService.getPostById(this.routeParams.id).then((response) => {
       console.log('response: ', response.content);
@@ -45,20 +39,8 @@ export class ViewingComponent implements OnInit {
       this.contentArray = [];
       temp.forEach(async (item) => {
         this.contentArray.push(item);
-        if (item.mediaType.includes('image/') || item.mediaType.includes('application/pdf')) {
-          //
-        } else if (item.mediaType === 'LinkID') {
-          this.videoArray.push(item);
-        }
       });
     });
-
-    // Make empty divs/containers given how many contents there are in the contentArray
-
-  }
-
-  sanitize(url: string) {
-    return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
   getRouteParams() {
@@ -73,74 +55,4 @@ export class ViewingComponent implements OnInit {
       this.queryParams = params;
     });
   }
-
-  previewImage() {
-    const curr = this.imageArray.shift();
-    const reader = new FileReader();
-    reader.readAsDataURL(curr.mediaObj);
-    let base64data;
-    reader.onloadend = () => {
-      base64data = reader.result;
-    };
-    this.imageURL = this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + base64data);
-    console.log(this.imageURL);
-  }
-
-  previewFile() {
-    var container = this.imageArray.shift();
-    // var preview = document.querySelector(container.mediaType);
-    var file = document.querySelector(container.mediaObj);
-    var reader = new FileReader();
-    reader.readAsDataURL(container.mediaObj);
-    var base64data;
-    reader.onloadend = function () {
-      base64data = reader.result;
-    };
-    // this.imageURL = this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + base64data);
-
-
-    var temp;
-    console.log('hello');
-
-    reader.addEventListener('load', function () {
-      temp = reader.result;
-    }, false);
-    this.imageURL = temp;
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  }
-
-  // function previewFiles() {
-
-  //   var preview = document.querySelector('#preview');
-  //   var files   = document.querySelector('input[type=file]').files;
-
-  //   function readAndPreview(file) {
-
-  //     // Make sure `file.name` matches our extensions criteria
-  //     if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
-  //       var reader = new FileReader();
-
-  //       reader.addEventListener("load", function () {
-  //         var image = new Image();
-  //         image.height = 100;
-  //         image.title = file.name;
-  //         image.src = this.result;
-  //         preview.appendChild( image );
-  //       }, false);
-
-  //       reader.readAsDataURL(file);
-  //     }
-
-  //   }
-
-  //   if (files) {
-  //     [].forEach.call(files, readAndPreview);
-  //   }
-
-  // }
-
-  /* Converts BLOB string from mongoDB to a local resource BLOB - used for image and pdf previews */
 }
